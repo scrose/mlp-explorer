d551a66a5e4f910d9220a06e188b874ad0db2da0
Spencer Rose <spencerrose@uvic.ca>
2023-07-29 17:15:19 -0700

    Image Toolkit upgrade to v2.0

--- transcoder/app.js
+++ transcoder/app.js
@@ -42,35 +42,43 @@ app.listen(port, () => {
  * @private
  */
 
-let queue = new Queue('transcode', {
-    redis: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-    },
-});
-
-queue.process(async (job) => {
-
-    const { src='' } = job.data || {};
-
-    console.log(`Processing job: ${job.id} / File: ${src}`);
-
-    transcode(job.data, console.log)
-        .then(res => {
-            console.log(`[Completed] Job: ${job.id} / File: ${src}`);
-
-            // heapdump.writeSnapshot(function(err, filename) {
-            //     console.log('dump written to', filename);
-            // });
-
-        })
-        .catch(console.error)
-        .finally(() => {
-            console.log('Garbage Collection ...')
-        // force garbage collection
-        if (typeof global.gc != "undefined") {
-            const gcRes = global.gc();
-            console.log('\t- [Node] Force GC:', gcRes);
-        }
+try {
+    let queue = new Queue('transcode', {
+        redis: {
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+        },
     });
-});
+
+    // Removes everything but only if there are no active jobs
+    await queue.obliterate();
+
+    queue.process(async (job) => {
+
+        const {src = ''} = job.data || {};
+
+        console.log(`Processing job: ${job.id} / File: ${src}`);
+
+        transcode(job.data, console.log)
+            .then(res => {
+                console.log(`[Completed] Job: ${job.id} / File: ${src}`);
+
+                // heapdump.writeSnapshot(function(err, filename) {
+                //     console.log('dump written to', filename);
+                // });
+
+            })
+            .catch(console.error)
+            .finally(() => {
+                console.log('Garbage Collection ...')
+                // force garbage collection
+                if (typeof global.gc != "undefined") {
+                    const gcRes = global.gc();
+                    console.log('\t- [Node] Force GC:', gcRes);
+                }
+            });
+    });
+}
+catch (err) {
+    console.error(err);
+}
