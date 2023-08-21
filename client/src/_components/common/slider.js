c6ee04abb2faa86c678128a3ff06570f199faab4
Spencer Rose <spencerrose@uvic.ca>
2023-08-20 17:51:47 -0700

    Redo image slider.

--- client/src/_components/common/slider.js
+++ client/src/_components/common/slider.js
@@ -1,17 +1,28 @@
 /*!
  * MLE.Client.Components.Common.Slider
  * File: slider.js
- * Copyright(c) 2022 Runtime Software Development Inc.
+ * Copyright(c) 2023 Runtime Software Development Inc.
  * Version 2.0
  * MIT Licensed
+ *
+ * ----------
+ * Description
+ *
+ * Sliding view of overlapping images
+ *
+ * ---------
+ * Revisions
+ * - 14-07-2023   Redo of slider canvases.
  */
 
-import React from 'react';
+import React, {useLayoutEffect, useRef, useState} from 'react';
 import { schema } from '../../schema';
 import Loading from './loading';
 import { UserMessage } from './message';
-import { scaleToFit } from '../toolkit/tools/scaler.toolkit';
-import Button from './button';
+import {scaleToFit} from '../toolkit/tools/scaler.toolkit';
+import InputSelector from "../selectors/input.selector";
+import Canvas from "../toolkit/canvas/default.canvas.toolkit";
+import styles from '../styles/slider.module.css';
 
 /**
  * Image slider component.
@@ -25,227 +36,236 @@ const Slider = ({ images = [], canvasWidth = 600, canvasHeight = 500 }) => {
     // input image data
     const [image1, image2] = images || [];
 
-    // warning/error messages
-    const [message, setMessage] = React.useState(null);
-
-    // loading status
-    const [status, setStatus] = React.useState(0);
-    const [loaded1, setLoaded1] = React.useState(false);
-    const [loaded2, setLoaded2] = React.useState(false);
-    const [imgOverlayWidth, setImgOverlayWidth] = React.useState(0);
-    const [imgOffset, setImgOffset] = React.useState(0);
-
-    // slider state
-    let sliding = false;
-
     // mounted status
     const _isMounted = React.useRef(false);
 
-    // panel DOM references
-    const panel1Ref = React.useRef();
-    const panel2Ref = React.useRef();
-    const canvas1Ref = React.useRef();
-    const canvas2Ref = React.useRef();
+    // image data layers
+    const containerRef = useRef();
+    const renderLayer1 = useRef(null);
+    const renderLayer2 = useRef(null);
+    const imageLayer1 = useRef(null);
+    const imageLayer2 = useRef(null);
+    const controlLayer = useRef();
 
-    // image DOM references
-    const img1Ref = React.useRef();
-    const img2Ref = React.useRef();
+    // create reference to panel resizer
+    let sliding = false;
+    let slideWidth = 0;
+    let slideInit = 0;
 
-    // slider DOM reference: element tools display of overlay
-    const sliderRef = React.useRef();
+    const [status1, setStatus1] = React.useState('empty');
+    const [status2, setStatus2] = React.useState('empty');
+    const [message, setMessage] = React.useState('empty');
+
+    const [viewWidth, setViewWidth] = React.useState(0);
+    const [viewDims, setViewDims] = React.useState(null);
 
     // Image labels
     const label1 = image1 && image1.hasOwnProperty('label') ? image1.label : 'Image 1';
     const label2 = image2 && image2.hasOwnProperty('label') ? image2.label : 'Image 2';
 
-    function _slideFinish() {}
+    // top layer opacity state
+    const [opacity, setOpacity] = useState(100);
 
-    function _slideMove(e) {
-        /* If the slider is no longer clicked, exit this function: */
-        if (!sliding) return false;
-        // get image boundary dimensions / positions
-        const rect = canvas1Ref.current.getBoundingClientRect();
 
-        // scale mouse coordinates after they have been adjusted to be relative to element
-        // - Prevent the slider from being positioned outside the image.
-        let x = Math.max(Math.min(Math.floor(e.clientX - rect.left), imgOverlayWidth + imgOffset ), imgOffset) ;
+    /**
+     * Load image data in comparator panel
+     *
+     * @private
+     */
 
-        /* Execute a function that will resize the overlay image according to the cursor: */
-        panel1Ref.current.style.width = x + 'px';
-        sliderRef.current.style.left = x - (sliderRef.current.offsetWidth / 2) + 'px';
+    const load = () => {
+
+        // status = image loading has started
+        setStatus1('loading');
+        setStatus2('loading');
+
+        // set data urls for images
+        const url1 = image1 && image1.hasOwnProperty('url') ? image1.url.medium : image1;
+        const url2 = image2 && image2.hasOwnProperty('url') ? image2.url.medium : image2;
+        // set image sources
+        imageLayer1.current.src = url1;
+        imageLayer2.current.src = url2;
+
+        // load image 1 (overlay)
+        imageLayer1.current.onerror = () => {
+            setMessage({ msg: 'Error: Image could not be loaded.', type: 'error' });
+            imageLayer1.current.src = schema.errors.image.fallbackSrc;
+        };
+        imageLayer1.current.onload = function() {
+
+            // compute scaled dimensions to fit view canvas
+            const viewDims1 = scaleToFit(imageLayer1.current.width, imageLayer1.current.height, canvasWidth, canvasHeight);
+
+            // load data into canvas layer
+            // - initially show half of top layer image
+            const initWidth = viewDims1.w / 2;
+            renderLayer1.current.draw(imageLayer1.current, {
+                view: {x: 0, y: 0, w: viewDims1.w / 2, h: viewDims1.h},
+                source: {x: 0, y: 0, w: imageLayer1.current.width / 2, h: imageLayer1.current.height}
+            });
+
+            // set initial view dims
+            setViewWidth(initWidth);
+            setViewDims(viewDims1);
+
+            // update load status
+            setStatus1('loaded');
+
+        };
+
+        // load image 2 (underlay)
+        imageLayer2.current.onerror = () => {
+            setMessage({ msg: 'Error: Image could not be loaded.', type: 'error' });
+            imageLayer2.current.src = schema.errors.image.fallbackSrc;
+        };
+        imageLayer2.current.onload = function() {
+            // compute scaled dimensions to fit view canvas
+            const viewDims2 = scaleToFit(
+                imageLayer2.current.width,
+                imageLayer2.current.height,
+                canvasWidth,
+                canvasHeight,
+            );
+
+            // load data into canvas layer
+            renderLayer2.current.draw(imageLayer2.current, {
+                view: {x: 0, y: 0, w: viewDims2.w, h: viewDims2.h},
+                source: {x: 0, y: 0, w: imageLayer2.current.width, h: imageLayer2.current.height}
+            });
+        };
+
+        setStatus2('loaded');
     }
 
-    // trigger redraw of canvas
-    React.useEffect(() => {
-        setStatus(0);
-    }, [images, setStatus, canvasWidth, canvasHeight]);
+    /**
+     * change top layer opacity
+     *
+     * @private
+     */
 
-    // initialize image sources
-    // - (1) data URLs
-    // - (2) URLs
-    React.useEffect(() => {
+    const _updateOpacity = (e) => {
+        const { target = {} } = e || {};
+        const { value = 100 } = target;
+        setOpacity(value);
+        // set canvas layer alpha value for opacity
+        renderLayer1.current.alpha( value / 100 );
+    }
 
-        _isMounted.current = true;
+    /**
+     * Load comparator images
+     * - comparison of images in same scale
+     * - use the largest dimension to set the scale
+     *
+     * @private
+     */
 
-        if (
-            _isMounted.current
-            && image1
-            && image2
-            && panel1Ref.current
-            && panel2Ref.current
-            && img1Ref.current
-            && img2Ref.current
-            && canvas1Ref.current
-            && canvas2Ref.current
-            && status === 0
-        ) {
-            // status = image loading has started
-            setStatus(1);
-
-            const img1 = img1Ref.current;
-            const img2 = img2Ref.current;
-            const panel1 = panel1Ref.current;
-            const panel2 = panel2Ref.current;
-            const canvas1 = canvas1Ref.current;
-            const canvas2 = canvas2Ref.current;
-            const ctx1 = canvas1.getContext('2d');
-            const ctx2 = canvas2.getContext('2d');
-            const slider = sliderRef.current;
-
-            // set data urls for images
-            const url1 = image1 && image1.hasOwnProperty('url')
-                ? image1.url.medium : image1;
-            const url2 = image2 && image2.hasOwnProperty('url')
-                ? image2.url.medium : image2;
-
-            // load image 1 (overlay)
-            img1.onerror = () => {
-                setMessage({ msg: 'Error: Image could not be loaded.', type: 'error' });
-                img1.src = schema.errors.image.fallbackSrc;
-            };
-            img1.onload = function() {
-
-                // compute canvas dimensions and scale image to fit
-                canvas1.width = canvasWidth;
-                canvas1.height = canvasHeight;
-                const {w, h} = scaleToFit(img1.naturalWidth, img1.naturalHeight, canvasWidth, canvasHeight);
-                // compute dx offset needed to centre image on canvas
-                const offset1 = (canvasWidth - w) / 2;
-                setImgOffset(offset1);
-                // scaled image width
-                setImgOverlayWidth(w);
-
-                /* Initialize the width of overlay image to 50% coverage */
-                ctx1.drawImage(img1, offset1, 0, w, h);
-                panel1.style.width = (canvasWidth / 2) + 'px';
-                panel1.style.height = canvasHeight + 'px';
-
-                /* Position the slider in the middle */
-                slider.style.left = (canvasWidth / 2) - (slider.offsetWidth / 2) + 'px';
-                slider.style.top = (canvasHeight / 2) - (slider.offsetHeight / 2) + 'px';
-
-                setLoaded1(true);
-
-            };
-            // set image source
-            img1.src = url1;
-
-            // load image 2 (underlay)
-            img2.onerror = () => {
-                setMessage({ msg: 'Error: Image could not be loaded.', type: 'error' });
-                img2.src = schema.errors.image.fallbackSrc;
-            };
-            img2.onload = function() {
-
-                // compute scaled canvas dimensions and scale image to fit
-                canvas2.width = canvasWidth;
-                canvas2.height = canvasHeight;
-                const {w, h} = scaleToFit(img2.naturalWidth, img2.naturalHeight, canvasWidth, canvasHeight);
-                const offset2 = (canvasWidth - w) / 2;
-
-                ctx2.drawImage(img2, offset2, 0, w, h);
-                panel2.style.width = canvasWidth + 'px';
-                panel2.style.height = canvasHeight + 'px';
-
-                setLoaded2(true);
-            };
-            img2.src = url2;
+    useLayoutEffect(()=>{
+        if (status1 === 'empty' || status2 === 'empty') {
+            load();
         }
-        return () => {
-            _isMounted.current = false;
-        };
-    }, [
-        imgOverlayWidth,
-        setImgOverlayWidth,
-        imgOffset,
-        setImgOffset,
-        loaded1,
-        loaded2,
-        setLoaded1,
-        setLoaded2,
-        status,
-        setStatus,
-        canvasWidth,
-        canvasHeight,
-        image1,
-        image2
-    ]);
+        return ()=>{_isMounted.current = true;}
+
+    }, []);
+
+    /* Initialize panel resize */
+    function _resizeStart(e) {
+        /* if slider is no longer engaged, exit this function: */
+        if (sliding) return false;
+        sliding = true;
+        slideInit = e.pageX;
+        slideWidth = viewWidth;
+    }
+
+    /* Position the slider and resize panel */
+    function _resizeEnd() {
+        /* if slider is no longer engaged, exit this function: */
+        if (!sliding) return false;
+        sliding = false;
+        setViewWidth(slideWidth);
+    }
+
+    /* Position the slider and resize panel */
+    function _resize(e) {
+
+        /* if slider is no longer engaged, exit this function: */
+        if (!sliding) return false;
+
+        // compute adjusted widths
+        const _viewWidth = viewWidth - Math.round(slideInit - e.pageX);
+        const _imageWidth = Math.round(_viewWidth * ( imageLayer1.current.width / viewDims.w));
+
+        // load data into canvas layer
+        // - initially show half of top layer image
+        renderLayer1.current.draw(imageLayer1.current, {
+            view: {x: 0, y: 0, w: _viewWidth, h: viewDims.h},
+            source: {x: 0, y: 0, w: _imageWidth, h: imageLayer1.current.height}
+        });
+
+        slideWidth = _viewWidth;
+
+    }
 
     return <>
-        <UserMessage
-            closeable={true}
-            message={message}
-            onClose={() => {
-                setMessage(false);
-            }}
-        />
-        <div className={'slider'}>
-            { ( !loaded1 || !loaded2 ) && <Loading className={'centered'} overlay={false}/> }
-            <div
-                className={`slider-container`}
-                onMouseLeave={_slideFinish}
-                onMouseUp={() => {
-                    sliding = false;
-                }}
-                onMouseMove={_slideMove}
-            >
-                <div className={`slider-button`} ref={sliderRef}
-                    onTouchStart={() => {
-                        sliding = true;
-                    }}
-                    onTouchMove={_slideMove}
-                    onTouchEnd={(e) => {
-                        e.preventDefault();
-                    }}
-                    onMouseDown={() => {
-                        sliding = true;
-                    }}
-                >
-                    { loaded1 && loaded2 && <Button icon={'slide'}/> }
-                </div>
-                <div ref={panel1Ref} className={'slider-img overlay'}>
-                    <canvas width={canvasWidth} height={canvasHeight} ref={canvas1Ref} />
-                </div>
-                <div ref={panel2Ref} className={'slider-img'}>
-                    <canvas width={canvasWidth} height={canvasHeight} ref={canvas2Ref} />
-                </div>
+        <div className={styles.slider}>
+            { ( status1 !== 'loaded' || status2 !== 'loaded' ) && <Loading className={'centered'} overlay={false}/> }
+            { message && <UserMessage message={message} /> }
+            <div className={styles.container} style={{height: canvasHeight, width: canvasWidth, margin: 'auto'}}>
+                <Canvas
+                    ref={controlLayer}
+                    id={`slider_image_control`}
+                    className={styles.overlay}
+                    width={canvasWidth}
+                    height={canvasHeight}
+                    onMouseDown={_resizeStart}
+                    onTouchStart={_resizeStart}
+                    onMouseUp={_resizeEnd}
+                    onMouseMove={_resize}
+                    onTouchEnd={_resizeEnd}
+                    onTouchMove={_resize}
+                />
+                <Canvas
+                    className={styles.image1}
+                    ref={renderLayer1}
+                    id={`slider_image_layer_1`}
+                    width={canvasWidth}
+                    height={canvasHeight}
+                />
+                <Canvas
+                    className={styles.image2}
+                    ref={renderLayer2}
+                    id={`slider_image_layer_2`}
+                    width={canvasWidth}
+                    height={canvasHeight}
+                />
                 <img
                     style={{ 'display': 'none' }}
-                    ref={img1Ref}
+                    ref={imageLayer1}
                     crossOrigin={'anonymous'}
                     src={schema.errors.image.fallbackSrc}
                     alt={label1}
                 />
                 <img
                     style={{ 'display': 'none' }}
-                    ref={img2Ref}
+                    ref={imageLayer2}
                     crossOrigin={'anonymous'}
                     src={schema.errors.image.fallbackSrc}
                     alt={label2}
                 />
             </div>
+            <InputSelector
+                style={{width: '200px'}}
+                id={'layer_opacity'}
+                name={'layer_opacity'}
+                label={`Opacity ${opacity}%`}
+                type={'range'}
+                value={opacity}
+                min={0}
+                max={100}
+                onChange={_updateOpacity}
+            />
         </div>
     </>;
+
 };
 
 export default Slider;
